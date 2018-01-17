ee7b6b6f9ac458cbd4217c8077cb4cb120d6ed9e
Balsa <balsa.l@live.com>
2018-01-17 12:52:26 +0100

    Prvi commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCFt7Hxj-7r2INgS6T-ygQ2eiBRwa_9r5E';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+//Take this HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
